63d49060197dc160169b2e4ff75a0244eff2097d
lixingchenpixelogic <lixing.chen@pixelogicmedia.com>
2025-01-28 13:08:09 -0800

    selfworking

--- hardlink_folders.py
+++ hardlink_folders.py
@@ -1,15 +1,15 @@
 import os
 import re
 import datetime
+import logging
 
-def handle_files(file_name, patch_dir, base_files, base_dir, destination_dir):
-    match = re.search(r'(\d{7})', file_name)
-    print('match looks like',match)
+
+def handle_files(file_name, patch_dir, base_files, destination_dir):
+    match = re.search(r'(\d{7})', file_name) 
     if match:
         file_suffix = match.group(1)
         file_path = os.path.join(patch_dir, file_name)
 
-        # Search for corresponding file in base_dir
         corresponding_base_file = None
         for base_file in base_files:
             if file_suffix in base_file:
@@ -18,8 +18,10 @@ def handle_files(file_name, patch_dir, base_files, base_dir, destination_dir):
 
         if corresponding_base_file:
             destination_file_path = os.path.join(destination_dir, corresponding_base_file)
-            if not os.path.exists(destination_file_path):
-                os.link(file_path, destination_file_path)
+            if os.path.exists(destination_file_path):
+                print('Destination file exists, removing: %s', destination_file_path)
+                os.remove(destination_file_path)
+            os.link(file_path, destination_file_path)
 
 def create_folder_c(base_tiff, patch_tiff, destination_parent):
     current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -29,35 +31,29 @@ def create_folder_c(base_tiff, patch_tiff, destination_parent):
         os.makedirs(destination_folder)
         print('Folder created', destination_folder)
 
-    folders_to_handle = ['TXLS', 'TXTD']
+    folders_to_handle = ['TXTD','TXLS']
 
     for folder in folders_to_handle:
         base_dir = os.path.join(base_tiff, folder)
-        print('The base directories handling now is',base_dir)
+        print('The base directories handling now is', base_dir)
         patch_dir = os.path.join(patch_tiff, folder)
-        print('The patch directories handling now is',patch_dir)
+        print('The patch directories handling now is', patch_dir)
         destination_dir = os.path.join(destination_folder, folder)
         
-        if folder == 'TXLS' and not os.path.exists(patch_dir):
-            print('triggered no TXLS in BaseTIFF')
-            if not os.path.exists(destination_dir):
-                os.makedirs(destination_dir)
-            base_files = os.listdir(base_dir)
-            for base_file in base_files:
-                base_file_path = os.path.join(base_dir, base_file)
-                destination_file_path = os.path.join(destination_dir, base_file)
-                if not os.path.exists(destination_file_path) and os.path.isfile(base_file_path):
-                    os.link(base_file_path, destination_file_path)
-        else:
-            if not os.path.exists(destination_dir):
-                os.makedirs(destination_dir)
-            
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+
+        base_files = os.listdir(base_dir)
+        
+        for base_file in base_files:
+            base_file_path = os.path.join(base_dir, base_file)
+            destination_file_path = os.path.join(destination_dir, base_file)
+            os.link(base_file_path, destination_file_path)
+
+        if os.path.exists(patch_dir):
             patch_files = os.listdir(patch_dir)
-            base_files = os.listdir(base_dir)
-            
             for file_name in patch_files:
-                print('filename looks like',file_name)
-                handle_files(file_name, patch_dir, base_files, base_dir, destination_dir)
+                handle_files(file_name, patch_dir, base_files, destination_dir)
 
     print(f"Folder {destination_folder} created with hardlinks to assets from BaseTIFF and PatchTIFF.")
 
