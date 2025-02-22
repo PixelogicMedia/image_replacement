8efb7eb48b588ca79d94474e18180f0825928e00
Gerardo-Parker <gerardo.parker@pixelogicmedia.com>
2025-02-21 19:52:19 -0800

    Update hardlink_folders.py
    moved date creation to service not script
    changed number logic to regex
    changed files list to a set to speed up search
    added printing results to json payload for service

--- hardlink_folders.py
+++ hardlink_folders.py
@@ -1,67 +1,73 @@
-import os
-import re
-import datetime
-import argparse
+import os, datetime, re, argparse, json
 
 
-def handle_files(file_name, patch_dir, base_files, destination_dir):
+def get_numbers(file_name):
     file_name_no_ext = os.path.splitext(file_name)[0]
-    digits = ""
-    for char in reversed(file_name_no_ext):
-        if char.isdigit():
-            digits = char + digits
-        else:
-            break
+    found = re.findall('\d+$', file_name_no_ext)
+    if len(found) == 0:
+        return ""
+    digits = found[0]
     last_7_digits = digits[-7:]
     file_suffix = last_7_digits.zfill(7)
+    return file_suffix
 
+def handle_files(file_name, patch_dir, base_files, destination_dir):
+    file_number = get_numbers(file_name)
     file_path = os.path.join(patch_dir, file_name)
-
-    corresponding_base_file = None
-    for base_file in base_files:
-        if file_suffix in base_file:
-            corresponding_base_file = base_file
-            break
-
+    if base_files:
+        corresponding_base_file = base_files[file_number] if file_number in base_files else None
+    else:
+        corresponding_base_file = file_name
+        
     if corresponding_base_file:
         destination_file_path = os.path.join(destination_dir, corresponding_base_file)
         if os.path.exists(destination_file_path):
-            print('Destination file exists, removing: %s', destination_file_path)
             os.remove(destination_file_path)
         os.link(file_path, destination_file_path)
+        return 1
+    return 0
 
 def create_new_folder(base_tiff, patch_tiff, destination_parent,folders_to_handle):
-    current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    destination_folder = os.path.join(destination_parent, current_time)
-    
+    total_images = 0
+    total_patched = 0
+    if os.path.exists(destination_parent):
+        i = 1
+        while(os.path.exists(f'{destination_parent}_V{i}')):
+            i += 1
+        destination_folder = f'{destination_parent}_V{i}'
+    else:
+        destination_folder = destination_parent
+
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
         print('Folder created', destination_folder)
 
     for folder in folders_to_handle:
         base_dir = os.path.join(base_tiff, folder)
-        print('The base directories handling now is', base_dir)
         patch_dir = os.path.join(patch_tiff, folder)
-        print('The patch directories handling now is', patch_dir)
         destination_dir = os.path.join(destination_folder, folder)
-        
+
         if not os.path.exists(destination_dir):
             os.makedirs(destination_dir)
 
-        base_files = os.listdir(base_dir)
-        
-        for base_file in base_files:
-            base_file_path = os.path.join(base_dir, base_file)
-            destination_file_path = os.path.join(destination_dir, base_file)
-            os.link(base_file_path, destination_file_path)
+        base_files_set = {}
+        if os.path.exists(base_dir):
+            base_files = os.listdir(base_dir)
+            for files in base_files:
+                base_files_set[get_numbers(files)] = files
 
+            for base_file in base_files:
+                base_file_path = os.path.join(base_dir, base_file)
+                destination_file_path = os.path.join(destination_dir, base_file)
+                total_images += 1
+                os.link(base_file_path, destination_file_path)
+            
         if os.path.exists(patch_dir):
             patch_files = os.listdir(patch_dir)
             for file_name in patch_files:
-                print('Handling file', file_name)
-                handle_files(file_name, patch_dir, base_files, destination_dir)
+                total_patched += handle_files(file_name, patch_dir, base_files_set, destination_dir)
 
-    print(f"Folder {destination_folder} created with hardlinks to assets from BaseTIFF and PatchTIFF.")
+    print("|JSTART|"+json.dumps({"output": destination_folder, "totalmatched": "", "totalimages": total_images})+"|JEND|")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some TIFF files.")
